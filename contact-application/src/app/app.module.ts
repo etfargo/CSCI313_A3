69d4d8f30b4c76718a0f7c17d7f1937d10fbf70f
etfargo <etfargo@hotmail.com>
2021-04-17 13:36:13 -0500

    Various Component Initialization

--- contact-application/src/app/app.module.ts
+++ contact-application/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact-list/contact/contact.component';
+import { AddContactComponent } from './contact-list/add-contact/add-contact.component';
+import { EditContactComponent } from './contact-list/edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactComponent,
+    AddContactComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule
